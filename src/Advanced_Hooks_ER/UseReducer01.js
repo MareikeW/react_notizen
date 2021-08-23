ef53ec7eab323982a0ecf173574cd77b884c69a2
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-23 10:05:43 +0200

    notizen useReducer 01

---
+++ src/Advanced_Hooks_ER/UseReducer01.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+// 1. Argument: aktueller Wert von count
+// 2. Argument: Wert, der an setCount weitergeben wird
+const countReducer = (state, newState) => newState;
+
+function Counter({initialCount = 0, step = 1}) {
+  // const [count, setCount] = React.useState(initialCount); --> useState
+  const [count, setCount] = React.useReducer(countReducer, initialCount);
+
+  const increment = () => setCount(count + step);
+  return <button onClick={increment}>{count}</button>;
+}
+
+function UseReducer01() {
+  return <Counter />
+}
+
+export default UseReducer01;
